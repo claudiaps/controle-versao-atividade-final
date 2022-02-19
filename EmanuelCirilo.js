698f61c0eec1f8a9483834f681004442d895188e
EmanuelCirilo <emanuel_janipa2@hotmail.com>
2022-02-18 23:25:59 -0300

    Desenvolvido uma atividade HTML e outra JS para a avaliação final.

---
+++ EmanuelCirilo.js
@@ -0,0 +1,11 @@
+var numero1 = prompt('Digite um número: ');
+numero1 = parseFloat(numero1);
+
+var numero2 = prompt('Digite outro número: ');
+numero2 = parseFloat(numero2);
+
+var numero3 = prompt('Digite o terceiro número: ');
+numero3 = parseFloat(numero3);
+
+var media = ((numero1+numero2+numero3)/3);
+alert('A media da soma dos três valores é igual à '+media);
